69237cfa3e6236a6a560a44448abb791edb0f499
Cesar Vaz <cesar_augusto_perfil@hotmail.com>
2021-04-11 18:42:04 -0300

    challenge finalized

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
@@ -6,7 +7,17 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      const userAdmin = this.turnUserAdminUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(userAdmin);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
